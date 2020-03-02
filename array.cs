b545d0aea3468e2b00ccdf42238b534dc30eca35
sezin <rlatpwls15@naver.com>
2020-03-02 22:15:13 +0900

    Create array.cs

---
+++ array.cs
@@ -0,0 +1,20 @@
+// using System;
+
+// class array {
+//     public static void Main() {
+//         int [] vector;
+//         short[,] matrix;
+//         long[][] arrayOfArray;
+//         object[] myArray1, myArray2;
+
+//         vector = new int[100];
+//         matrix = new short[10, 100];
+//         myArray1 = new Point[3];
+
+//         for (int i=0; i<vector.Length; i++)
+//         {
+//             vector[i]=i;
+//             Console.WriteLine(""+vector[i]);
+//         }
+//     }
+// }
