07065b34e8b74896dbec5d8f643f27d571ea8b2a
sezin <rlatpwls15@naver.com>
2020-03-02 22:15:49 +0900

    Create OperatorOverloadingApp.cs

---
+++ Studying_csharp/OperatorOverloadingApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studying_csharp
+{
+    class Even
+    {
+        int evenNumber;
+        public Even(int n)
+        {
+            evenNumber = (n % 2 == 0) ? n : n + 1;
+        }
+        public static Even operator++(Even e)
+        {
+            e.evenNumber += 2;
+            return e;
+        }
+        public static Even operator--(Even e)
+        {
+            e.evenNumber -= 2;
+            return e;
+        }
+        public void PrintEven()
+        {
+            Console.WriteLine("Even Number =" + evenNumber);
+        }
+
+    }
+
+    class OperatorOverloadingApp
+    {
+        public static void Main()
+        {
+            Even e = new Even(4); e.PrintEven();
+            ++e;                  e.PrintEven();
+            --e;                  e.PrintEven();
+        }
+    }
+}
