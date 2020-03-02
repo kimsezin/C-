cc1ae9eaf8dec3c62c8c9c69e43ee0c10e4a39ee
sezin <rlatpwls15@naver.com>
2020-03-02 22:16:20 +0900

    Create DelegateApp.cs

---
+++ Studying_csharp/DelegateApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studying_csharp
+{
+    delegate void SampleDelegate(); //델리게이트 정의
+    class DelegateClass
+    {
+        public void DelegateMethod() //델리게이트할 메소드
+        {
+            Console.WriteLine("In the DelegateClass.DelegateMethod ...");
+        }
+    }
+    class DelegateApp
+    {
+        public static void Main()
+        {
+            DelegateClass obj = new DelegateClass();
+            SampleDelegate sd = new SampleDelegate(obj.DelegateMethod);
+            sd();
+        }
+    }
+}
