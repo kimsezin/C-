1d34828ba9b84c6c0b440e25a8216c52aa239691
sezin <rlatpwls15@naver.com>
2020-03-02 22:31:24 +0900

    Thread

---
+++ Studying_csharp/ThreadApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Studying_csharp
+{
+    class ThreadApp
+    {
+        static void ThreadBody()
+        {
+            Console.WriteLine("in th thread body ...");
+        }
+        public static void Main()
+        {
+            ThreadStart ts = new ThreadStart(ThreadBody);
+            Thread t = new Thread(ts);
+            Console.WriteLine("*** start of main");
+            t.Start();
+            Console.WriteLine("*** end of main");
+        }
+    }
+}
