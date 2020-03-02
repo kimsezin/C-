a005c1db7f4c2506c47be639804b1b7447aca349
sezin <rlatpwls15@naver.com>
2020-03-02 22:16:18 +0900

    Create EventApp.cs

---
+++ Studying_csharp/EventApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studying_csharp
+{
+    class EventApp
+    {
+        public EventHandler MyEvent;
+        void MyEventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World");
+        }
+        public EventApp()
+        {
+            this.MyEvent += new EventHandler(MyEventHandler);
+        }
+        public void InvokeEvent()
+        {
+            if (MyEvent != null)
+                MyEvent(this, null);
+        }
+        public static void Main()
+        {
+            new EventApp().InvokeEvent();
+        }
+    }
+}
