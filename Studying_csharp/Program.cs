695dccfa8a43e8e77f7049bb66e71f1cda852d48
sezin <rlatpwls15@naver.com>
2020-03-02 22:15:47 +0900

    Create Program.cs

---
+++ Studying_csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class PropertyClass
+{
+    private int privateValue;
+    public int PrivateValue
+    {
+        get { return privateValue; }
+        set { privateValue = value; }
+    }
+    public void PrintValue()
+    {
+        Console.WriteLine("Hidden Value = " + privateValue);
+    }
+}
+
+class PropertyApp
+{
+    public static void Main()
+    {
+        int n;
+        PropertyClass obj = new PropertyClass();
+        obj.PrivateValue = 1000;
+        obj.PrintValue();
+        n = obj.PrivateValue;
+        Console.WriteLine("      value = "+n);
+
+    }
+}
